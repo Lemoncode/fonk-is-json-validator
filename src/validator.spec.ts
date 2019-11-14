c6b61aad4fee54cb796da6ce5c24446793bc18d2
Nya Garcia <nyablk97@gmail.com>
2019-11-14 23:38:25 +0100

    feat(validator.spec): add specs

--- src/validator.spec.ts
+++ src/validator.spec.ts
@@ -1,82 +1,224 @@
-import { validator, setErrorMessage } from './validator';
+import { setErrorMessage, validator } from './validator';
 
-// TODO: Add specs
 describe('fonk-is-json-validator specs', () => {
-  it('should return succeeded validation when it feeds value equals undefined', () => {
-    // Arrange
-    const value = void 0;
+  it('should return succeeded validation when value is a valid JSON', () => {
+    const value = '{ "name": "John", "age": 30, "car": null }';
 
-    // Act
     const result = validator({ value });
 
-    // Assert
     expect(result).toEqual({
       succeeded: true,
       message: '',
-      type: '',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid JSON', () => {
+    const value = '{ "fruit": "Apple", "size": "Large", "color": "Red"}';
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid JSON', () => {
+    const value =
+      '{ "object": { "a": "bau bau", "c": "boo boo", "e": "mrau mrau" }, "array": [1, 2, 3], "string": "Foo bar" }';
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return succeeded validation when value is a valid JSON', () => {
+    const value =
+      '{ "quiz": { "sport": { "q1": { "question": "Which one is correct team name in NBA?", "options": ["New York Bulls", "Los Angeles Kings", "Golden State Warriros", "Huston Rocket"], "answer": "Huston Rocket" } }, "maths": {"q1": { "question": "5 + 7 = ?", "options": [10, 11, 12, 13], "answer": 12 }, "q2": { "question": "12 - 8 = ?", "options": [1, 2, 3, 4], "answer": 4 } } } }';
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return succeeded validation when it feeds value equals undefined', () => {
+    const value = void 0;
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: true,
+      message: '',
+      type: 'IS_JSON',
     });
   });
 
   it('should return succeeded validation when it feeds value equals null', () => {
-    // Arrange
     const value = null;
 
-    // Act
     const result = validator({ value });
 
-    // Assert
     expect(result).toEqual({
       succeeded: true,
       message: '',
-      type: '',
+      type: 'IS_JSON',
     });
   });
 
   it('should return succeeded validation when it feeds value equals empty string', () => {
-    // Arrange
     const value = '';
 
-    // Act
     const result = validator({ value });
 
-    // Assert
     expect(result).toEqual({
       succeeded: true,
       message: '',
-      type: '',
+      type: 'IS_JSON',
+    });
+  });
+
+  it("should return failed validation when value isn't a valid JSON", () => {
+    const value = { name: 'John', age: 30, car: null };
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it("should return failed validation when value isn't a valid JSON", () => {
+    const value = '{ name: "John", age: 30, car: null }';
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value equals {}', () => {
+    const value = {};
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value equals []', () => {
+    const value = [];
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it("should return failed validation when value isn't a valid JSON", () => {
+    const value = [{ name: 'Nya' }];
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value equals false', () => {
+    const value = false;
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value equals true', () => {
+    const value = true;
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value is an integer', () => {
+    const value = 12;
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
+    });
+  });
+
+  it('should return failed validation when value is a string', () => {
+    const value = 'foo';
+
+    const result = validator({ value });
+
+    expect(result).toEqual({
+      succeeded: false,
+      message: 'Provided value is not a valid JSON',
+      type: 'IS_JSON',
     });
   });
 
   it('should overwrite default message when it feeds value and message', () => {
-    // Arrange
     const value = 'test';
     const message = 'other message';
 
-    // Act
     const result = validator({ value, message });
 
-    // Assert
     expect(result).toEqual({
       succeeded: false,
       message: 'other message',
-      type: '',
+      type: 'IS_JSON',
     });
   });
 
   it('should overwrite default message when it feeds value and calls to setErrorMessage', () => {
-    // Arrange
     const value = 'test';
 
     setErrorMessage('other message');
 
-    // Act
     const result = validator({ value });
 
-    // Assert
     expect(result).toEqual({
       succeeded: false,
       message: 'other message',
-      type: '',
+      type: 'IS_JSON',
     });
   });
 });
